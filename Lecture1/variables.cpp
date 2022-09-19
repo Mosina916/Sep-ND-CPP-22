45fee5dbc4ed8064831f250ef545864453c98b65
Mosina916 <mosinaashraf43@gmail.com>
2022-09-19 18:04:45 +0530

    Basics of CPP

---
+++ Lecture1/variables.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){//start
+	// cin>>-->to take input
+	int p,r,t;
+	// cin>>p;
+	// cin>>r;
+	
+	// // cin>>t;
+	// cin>>p>>r>>t;
+	// int si=p*r*t/100;
+	// cout<<si<<endl;
+
+
+	// int a763neh=67;
+	// int Y763neh=67;
+	// int _763neh=67;
+	int 763neh=67;
+	cout<<763neh<<endl;
+
+
+	return 0;//end
+}
---
+++ Lecture2/conditionals.cpp
@@ -0,0 +1,138 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// single if
+
+	// if(condition){
+	// 	// task you want to done if true
+
+	// }
+
+	// int age=30;
+	// if(age==20){
+	// 	cout<<"i will gift you a car"<<endl;
+	// }
+
+
+	// if(condition){
+	// 	// con true
+
+	// }
+	// else{
+	// 	// cond false
+	// }
+	// int age=90;
+	// if(age==30){
+	// 	cout<<"hello world"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"coding blocks"<<endl;
+
+	// }
+
+	// if(condition){
+
+	// }
+	// else if(condition){
+
+	// }
+	// else if(condition){
+
+	// }
+	// else{
+
+	// }
+
+	// int marks=78;
+	// int marks;
+	// cin>>marks;//83
+	// if( marks>=90 && marks<=100){
+
+	// 	cout<<"grade A"<<endl;
+	// }
+	// else if(marks>=80 and marks<=89){
+	// 	cout<<"grade B"<<endl;
+
+	// }
+	// else if(marks>=70 and marks<=79){
+	// 	cout<<"grade C"<<endl;
+
+	// }
+	// else if(marks>=60 and marks<=69){
+	// 	cout<<"grade D"<<endl;
+
+	// }
+	// else if(marks>=50 and marks<=59){
+	// 	cout<<"grade E"<<endl;
+
+	// }
+	// else{
+	// 	cout<<"Fail"<<endl;
+
+	// }
+
+
+
+
+	// // multiple if
+	// int age;
+	// cin>>age;//20
+
+	// if(age>=20){
+	// 	cout<<"hello"<<endl;
+
+	// }
+	// if(age==20){
+	// 	cout<<"world"<<endl;
+	// }
+	
+
+	// if(age>=20){
+	// 	cout<<"hello"<<endl;
+
+	// }
+	// else if(age==20){
+	// 	cout<<"world"<<endl;
+	// }
+	// else{
+	// 	cout<<"coding"<<endl;
+	// }
+
+
+	// if(-13){
+	// 	cout<<"hello"<<endl;
+	// }
+
+	// if(0){
+	// 	cout<<"hello"<<endl;
+	// }
+
+
+	// int age=89;
+	// if(age>=80 || age>=90){
+	// 	cout<<"hello"<<endl;
+	// }
+	// else{
+	// 	cout<<"world"<<endl;
+
+	// }
+
+
+	// if(not 0){
+	// 	cout<<"hello"<<endl;
+	// }
+
+	// if(3 and 4){
+	// 	cout<<"coding blocks"<<endl;
+	// }
+	if(0 and 4){
+		cout<<"coding blocks"<<endl;
+	}
+
+
+
+
+
+	return 0;
+}
