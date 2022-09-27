0fb4d89bc61d8f1031631ee5c92db26dcb01d76a
Mohd Ehtesham <iamehtesham09@gmail.com>
2022-09-27 13:26:50 +0530

    lecture 3 and 4

---
+++ Lecture4/pattern3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int trows;
+	cin>>trows;
+	int rowno=1;
+	while(rowno<=trows){
+		// ==============================
+		int startval=1;
+	int place=1;
+
+	while(place<=rowno){
+		// cout<<"* ";
+		cout<<startval<<" ";
+		startval=startval+1;
+	place=place+1;//2
+	}
+	cout<<endl;
+	rowno=rowno+1;
+	
+	// ==============================
+
+
+	}
+
+
+
+
+
+	// row no 3
+	
+	return 0;
+}
